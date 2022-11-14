1421cc360e8775aec71409af2287e648a5daa193
Himesh Maniyar <himeshmaniyar2017@gmail.com>
2022-11-14 23:48:00 +0530

    Removed force update package

--- frontend/src/Components/LeftNavBtns.js
+++ frontend/src/Components/LeftNavBtns.js
@@ -1,14 +1,11 @@
 import React from "react";
-import useForceUpdate from "use-force-update";
 
 const LeftNavBtns = ({ name, setTable }) => {
-  const forceUpdate = useForceUpdate();
   return (
     <div className="DBelector">
       <button
         onClick={() => {
           setTable(name);
-          forceUpdate();
         }}
       >
         {name}
